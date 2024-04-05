f5ad6c41d197cbf3b103de4f11d4f52740ec4893
Agustin Morro <agustin.morro@gmail.com>
2024-04-05 13:54:57 -0300

    changeimgs

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
             <img className="max-w-28 md:max-w-32" src={escudohome3} alt="" />
           </header>
           <picture className="z-10 w-full flex justify-center items-end md:absolute md:bottom-0 md:mx-auto">
-            <img  loading="lazy" className="img-home max-w-72 md:max-w-72 lg:max-w-80 lg:ml-44" src={imgHome} alt="" />
+            <img  loading="lazy" className="img-home mt-6 max-w-72 md:max-w-72 lg:max-w-80 lg:ml-44" src={imgHome} alt="" />
           </picture>
         </section>
 
