8b6685c98fe8810008bcc530c22bbda9c3898957
Agustin Morro <agustin.morro@gmail.com>
2024-04-05 01:20:45 -0300

    fix-home

--- src/App.jsx
+++ src/App.jsx
@@ -195,11 +195,11 @@ function App() {
           </picture>
         </section>
 
-        <section>
-          <picture className="w-full flex justify-center items-center my-14">
+        <section className="w-full flex flex-col justify-center items-center">
+          <picture className="w-full flex justify-center items-center my-12">
             <img className="max-w-64 lg:max-w-96" src={suelaP} alt="" />
           </picture>
-          <picture className="w-[95vw] my-6 md:w-4/5 md:my-12 lg:mb-24 flex justify-center">
+          <picture className="w-[95vw] my-2 md:w-4/5 md:my-12 lg:mb-24 flex justify-center self-center lg:-mt-12 ">
             <img src={imgFooter} alt="" />
           </picture>
         </section>
